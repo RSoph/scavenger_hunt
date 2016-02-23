78378484e96d9b1bd0fc717cd3a0d88720f7b036
RSoph <kate.brender@gmail.com>
2016-02-23 17:35:02 -0500

    ended question sequence

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -4,14 +4,17 @@ class QuestionController < ApplicationController
 		if params[:number]
 			number = params[:number].to_i
 			@question = Question.find_by(number: number)
-			if params[:answer] == @question.answer
+			if params[:answer] && (params[:answer].downcase.strip == @question.answer)
 				number += 1
+				if number == 8
+					render :last and return
+				end
 			else
 				flash[:notice] = "Oops, try again!"
 			end
-		# number ||= 1
-		@question = Question.find_by(number: number)
 		end
+		number ||= 1
+		@question = Question.find_by(number: number)
 	end
 
 	private
