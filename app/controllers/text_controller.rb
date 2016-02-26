ea4568de61f186ad9a2f518a2c014b2a1fd0c4a1
RSoph <kate.brender@gmail.com>
2016-02-25 20:47:32 -0500

    added real questions to the texting portion

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,22 +1,34 @@
 class TextController < ApplicationController
 	def index
 
-		@question = [ "text of question 1",
-					  "text of question 2",
-					  "text of question 3",
-					  "text of question 4"
+		@question = [ "In 1990, a bank branch opened in Park Slope, which you said was so ugly it belonged in Manhattan, not Brooklyn. What is the address number of that bank branch?",
+					  "Go to the shortest street in the whole neighborhood. How many fire hydrants does it have?",
+					  "321 are consectutive integers, but a true Park Slopian knows that those numbers are usually prefixed by what two letters?",
+					  "Right. Now, how many steps down do you take to get to that school's front entrance?",
+					  "One last math problem: What is the smallest integer that can be expressed as the sum of a prime and a composite, and is itself a prime?",
+					  "What is the last name of the 20th US President? (feel free to look it up or call a friend)",
+					  "Looks like we're going to 7th and Garfield! When you get there, tell me the total number of stripes painted in the crosswalks at that intersection",
+					  "Congratulations you win! You've won a free lunch at Upstairs Mexican! But this offer is only good if you go in right now. Thanks for playing!"
 					]
-		@answer = 	[ "answer to question 1",
-					  "answer to question 2",
-					  "answer to question 3",
-					  "answer to question 4"
+		@answer = 	[ "110",
+					  "1",
+					  "ps",
+					  "5",
+					  "7",
+					  "garfield",
+					  "20",
+					  ""
 					]
-		@hint = 	[ "hint for question 1",
-					  "hint for question 2",
-					  "hint for question 3",
-					  "hint for question 4"
+		@hint = 	[ "It's on President street and 7th Ave",
+					  "The street is Polhemus!",
+					  "321 is the partial name of a school in the neighborhood",
+					  "I can't think of a good hint for this one. It's between 4 and 6.",
+					  "It's less than 10",
+					  "Also the name of a famous cartoon cat.",
+					  "It should be the same as your total number of fingers and toes.",
+					  ""
 					]
-		@error = ["I didn't get that, error!"]
+		@error = ["I didn't get that, could you try again? I do best if you leave off any punctuation and use numerals when you can."]
 
 		message_body = params["Body"]
 		message_body ||= "just testing"
@@ -34,16 +46,18 @@ class TextController < ApplicationController
 			)
 		end
 
-		if body == 'hunt' # beginning
-			question_number = 1
-	  		message(from_number, @question[0])
-	  		message('7187535492', 'first text')
+		if body == 'hunt'
+			message(from_number, "Welcome! Remember, you always can text 'hint' for a hint on your current question.")
+			question_number = 0
+			sleep(3)
+	  		message(from_number, @question[question_number])
+	  		message('7187535492', "texted question number #{(question_number.to_i + 1).to_s}")
 	  	elsif body == 'hint'
 			message(from_number, @hint[question_number])
 		elsif body == @answer[question_number]
 			question_number += 1
 			message(from_number, @question[question_number])
-			message('7187535492', "texted question number #{question_number}")
+			message('7187535492', "texted question number #{(question_number.to_i + 1).to_s}")
 		else
 			message(from_number, @error[0])
 		end
