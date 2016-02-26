f1affe487fe273f6452274155c1a514f2f6a05fa
RSoph <kate.brender@gmail.com>
2016-02-25 22:14:15 -0500

    changed clock to a sensible time and finalized dad_controller

--- app/controllers/dad_controller.rb
+++ app/controllers/dad_controller.rb
@@ -1,15 +1,10 @@
 class DadController < ApplicationController
 
 	def index
-		# if Time.now.to_s > "2016-02-29 13:33:25 -0500"
-		if Time.now.to_s < "2016-02-29 13:33:25 -0500"
+		if Time.now.to_s < "2016-03-01 12:00:00 -0500"
 			render :not_yet
 		else
-			# @question = Question.find_by(number: "1")
 			redirect_to questions_path({number: 1})
 		end
-	end
-
-	def start
 	end
 end
