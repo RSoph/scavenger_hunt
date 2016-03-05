a1d16517eb6c0db0b099a35830be092bf0b84a85
RSoph <kate.brender@gmail.com>
2016-03-04 19:18:45 -0500

    opened the game so I could test live

--- app/controllers/dad_controller.rb
+++ app/controllers/dad_controller.rb
@@ -1,7 +1,7 @@
 class DadController < ApplicationController
 
 	def index
-		if Time.now.to_s < "2016-03-05 12:00:00 -0500"
+		if Time.now.to_s > "2016-03-05 12:00:00 -0500"
 			render :not_yet
 		else
 			redirect_to questions_path({number: 1})
